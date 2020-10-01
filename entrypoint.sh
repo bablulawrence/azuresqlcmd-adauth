3c726da39b0e2cc0f28f857eea8e9c08c4fb50ba
Bablu Lawrence <bablulawrence@outlook.com>
2020-10-01 05:11:59 +0000

    Fixed issues with script file paths, updated README

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if  [ -n "$INPUT_TENANT_ID" ] && \
     [ -n "$INPUT_QUERY_STRING" ] && \
     [ -n "$INPUT_RETURN_RESULT_FLAG" ]
 then 
-    ./Invoke-AzureSqlCmd.ps1 \
+    /Invoke-AzureSqlCmd.ps1 \
         -TenantId "$INPUT_TENANT_ID" \
         -ServicePrincipalId  "$INPUT_SERVICE_PRINCIPAL_ID" \
         -ServicePrincipalSecret "$INPUT_SERVICE_PRINCIPAL_SECRET" \
@@ -25,7 +25,7 @@ elif
     [ -n "$INPUT_QUERY_FILE" ] && \
     [ -n "$INPUT_RETURN_RESULT_FLAG" ]
 then 
-    ./Invoke-AzureSqlCmd.ps1 \
+    /Invoke-AzureSqlCmd.ps1 \
         -TenantId "$INPUT_TENANT_ID" \
         -ServicePrincipalId  "$INPUT_SERVICE_PRINCIPAL_ID" \
         -ServicePrincipalSecret "$INPUT_SERVICE_PRINCIPAL_SECRET" \
